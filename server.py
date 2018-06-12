0b3640a6bc4c475fb51110e9fb9977f81698594c
eduh1997 <eduardooliveira.1997@alunos.utfpr.edu.br>
2018-06-12 06:22:08 -0300

    nao precisa mais iniciar cliente antes do servidor

--- server.py
+++ server.py
@@ -10,26 +10,36 @@ import time
 class server(object):
 
     '''
-    No construtor da classe, definimos que diretório padrão é o atual, o host padrão é o que retorna da
-    função socket.gethostname() e a porta padrão é a 50007 que é uma porta não usada
+    No construtor da classe, definimos que diretorio padrao e o atual, o host padrao e o que retorna da
+    funcao socket.gethostname() e a porta padrao e a 50007 que e uma porta nao usada
     '''
     def __init__(self,dire = os.getcwd(), host = socket.gethostname(), port = 50007):
         self.eventos = []       #inicia um vetor que vai armazer todos os eventos
+        self.flag = 0           #flag para verificar se está conectado
         th = threading.Thread(target=notify.monitorar, args=(self.eventos,dire))    #inicia uma thread para monitorar e passa
                                                                                     # o vetor para ir colocando os eventos
         th.daemon = True      #variavel para a thread morrer junto com o processo
         th.start()            #inicia a thread
         time.sleep(1)         #espera a pasta ser criada na thread de monitorar
-        dire = os.getcwd()    #pega o diretório atual
-        dire += "/BkpSync"    #
-        os.chdir(dire)        #Muda o diretório do nosso programa para a pasta do BkpSync
+        dire = os.getcwd()    #pega o diretorio atual
+        dire += "/BkpSync"    #concatena com o nome da pasta
+        os.chdir(dire)        #Muda o diretorio do nosso programa para a pasta do BkpSync
         self.server = socket.socket()   #inicia um socket
-        self.server.connect((host, port))   #tenta conectar ao socket referenciado por parametro
-        self.send()                         #chama a função de mandar mensagem
+        while self.flag == 0:
+            th = threading.Thread(target=self.conectar, args=(host, port))
+            th.daemon = True
+            th.start()                          
+            time.sleep(1)
+        print "connected"
+        self.send()                         #chama a funcao de mandar mensagem
+
+    def conectar(self,host,port):
+        self.server.connect((host,port))
+        self.flag = 1
         
     
     def send(self):
-        while True:         #faz para sempre, até que o programa morra
+        while True:         #faz para sempre, ate que o programa morra
             while len(self.eventos):    #verifica se tem eventos
                 th = threading.Thread(target = self.event())    #cria uma thread para enviar o evento
                 th.daemon = True     #variavel para a thread morrer junto com o processo
@@ -38,18 +48,18 @@ class server(object):
     
     def event(self):
         mes = self.eventos.pop()    #passa para a variavel mes o evento que foi gerado
-        mes1 = mes                  #faz uma cópia desse evento
+        mes1 = mes                  #faz uma copia desse evento
         mes = util.format_message(mes)      #formata a mensagem em string para mandar pelo socket
         self.server.sendall(mes)            #envia a mensagem toda de controle
         print mes                           #printa a mensagem enviada
         print self.server.recv(1024)        #printa a resposta do cliente
-        if int(mes1['header'].mask) == notify.MODIFY_FILE or int(mes1['header'].mask) == notify.FILE_MOVE_TO:   #verifico se é uma modificação ou um move_to
+        if int(mes1['header'].mask) == notify.MODIFY_FILE or int(mes1['header'].mask) == notify.FILE_MOVE_TO:   #verifico se e uma modificacao ou um move_to
             message = util.decode_message(mes)      #decodifico a mensagem enviada
-            self.send_file(message)                 #chama a função de enviar arquivo
+            self.send_file(message)                 #chama a funcao de enviar arquivo
 
 
     def send_file(self, mes):
-        dire = mes[3] + '/' + mes[2]        #concatena diretório com o arquivo
+        dire = mes[3] + '/' + mes[2]        #concatena diretorio com o arquivo
         time.sleep(0.1)                     #evita o bug de ler o tamanho do arquivo como 0
         tam = os.path.getsize(dire)         #vejo o tamanho do arquivo
         self.server.sendall(str(tam))       #envio pro cliente o tamanho do arquivo que vou enviar
@@ -68,11 +78,11 @@ class server(object):
 if __name__ == "__main__":
 
     #<Diretorio> <hostname> <porta>
-    if len(sys.argv) == 1:      #se passar somente um argumento, ou seja, só o python server.py, chama o construtor sem nada
+    if len(sys.argv) == 1:      #se passar somente um argumento, ou seja, so o python server.py, chama o construtor sem nada
         s = server()      
-    elif len(sys.argv) == 2:    #se passar somente um argumento além do arquivo, deve ser o diretório
+    elif len(sys.argv) == 2:    #se passar somente um argumento alem do arquivo, deve ser o diretorio
         s = server(sys.argv[1])
-    elif len(sys.argv) == 3:    #se passar dois argumentos além do arquivo, deve ser o diretório e o hostname
+    elif len(sys.argv) == 3:    #se passar dois argumentos alem do arquivo, deve ser o diretorio e o hostname
         s = server(sys.argv[1], sys.argv[2])
-    elif len(sys.argv) == 4:    #se passar três argumentos além do arquivo, deve ser o diretório, o hostname e a porta
+    elif len(sys.argv) == 4:    #se passar três argumentos alem do arquivo, deve ser o diretorio, o hostname e a porta
         s = server(sys.argv[1], sys.argv[2], sys.argv[3])
